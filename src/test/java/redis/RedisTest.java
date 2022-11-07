8825aec4a017d89b2df7a0959e83a7af8eb6dcec
Walter Nuccio <walter.nuccio@agilelab.it>
2022-11-07 15:57:54 +0100

    add jedis and login

--- src/test/java/redis/RedisTest.java
+++ src/test/java/redis/RedisTest.java
@@ -1,10 +1,21 @@
 package redis;
 
 import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
 
 public class RedisTest {
     @Test
     void name() {
+        Jedis jedis = jedis();
+
+        System.out.println(jedis.info());
+
         assert(1 == 1);
+    }
+
+    private Jedis jedis() {
+        Jedis jedis = new Jedis("redis-19321.c240.us-east-1-3.ec2.cloud.redislabs.com", 19321);
+        jedis.auth("7vkXLfZRpRe5fyJ4kFM4wN0UHUJJQ76I");
+        return jedis;
     }
 }
