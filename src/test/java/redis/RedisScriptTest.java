36ab0edf7f38c5e2f8d1b72c3a16f4f87ab8c10b
Walter Nuccio <walter.nuccio@agilelab.it>
2022-11-16 12:53:49 +0100

    clarify the role of KEYS array

--- src/test/java/redis/RedisScriptTest.java
+++ src/test/java/redis/RedisScriptTest.java
@@ -18,18 +18,19 @@ public class RedisScriptTest {
 
     @Test
     void script_to_return_the_sum_of_two_arguments() {
-        String id = redis.scriptLoad("return ARGV[1] + ARGV[2]");
+        String scriptId = redis.scriptLoad("return ARGV[1] + ARGV[2]");
 
-        Object result = redis.evalsha(id, asList(""), asList("1", "2"));
+        Object result = redis.evalsha(scriptId, asList(""), asList("1", "2"));
 
         assertThat(result).isEqualTo(3L);
     }
 
     @Test
     void script_that_uses_KEYS_and_ARGV_arrays() {
-        String id = redis.scriptLoad("redis.call('SET', KEYS[1], ARGV[1])");
+        // Important: any 'key' inside the script must be specified within the KEYS array
+        String scriptId = redis.scriptLoad("redis.call('SET', KEYS[1], ARGV[1])");
 
-        Object result = redis.evalsha(id, asList("key"), asList("value"));
+        redis.evalsha(scriptId, asList("key"), asList("value"));
 
         assertThat(redis.get("key")).isEqualTo("value");
     }
